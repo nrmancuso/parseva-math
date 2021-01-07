db3daf8a27bb68ac046f2eb801d201730e9049d0
Nick Mancuso <nmancus1@dtcc.edu>
2021-01-07 01:25:00 -0500

    Issue #10: Use picoCLI to extend functionality
    Signed-off-by: Nick Mancuso <nmancus1@dtcc.edu>

--- src/main/java/parsevamath/tools/Main.java
+++ src/main/java/parsevamath/tools/Main.java
@@ -37,6 +37,12 @@ import org.antlr.v4.runtime.CommonTokenStream;
 
 import parsevamath.tools.grammar.MathLexer;
 import parsevamath.tools.grammar.MathParser;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.ParameterException;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.ParseResult;
 
 /**
  * Main driver class for parseva-math.
@@ -44,40 +50,125 @@ import parsevamath.tools.grammar.MathParser;
 public final class Main {
 
     private Main() {
-        // Prevent instantiation of Main
+        // prevent instantiation
     }
 
     /**
      * For now, this method runs only in "interactive" mode.
      *
-     * @param args need to implement
+     * @param args command line args
      * @noinspection UseOfSystemOutOrSystemErr
      */
     public static void main(String... args) {
-        final Scanner scan = new Scanner(System.in, StandardCharsets.UTF_8);
-        while (true) {
 
+        final CliOptions cliOptions = new CliOptions();
+        final CommandLine commandLine = new CommandLine(cliOptions);
+        commandLine.setUsageHelpWidth(CliOptions.HELP_WIDTH);
+        commandLine.setCaseInsensitiveEnumValuesAllowed(true);
+
+        try {
+            final ParseResult parseResult = commandLine.parseArgs(args);
+
+            if (parseResult.isUsageHelpRequested()) {
+                commandLine.usage(System.out);
+            }
+            else if (cliOptions.interactiveMode) {
+                runInteractive();
+            }
+            else if (cliOptions.evaluationMode) {
+                evaluate(cliOptions.expression);
+            }
+        }
+        catch (ParameterException parameterException) {
+            commandLine.usage(System.out);
+        }
+    }
+
+    /**
+     * This method handles the "interactive mode" loop where
+     * the user is prompted to enter an expression to be evaluated.
+     *
+     * @noinspection UseOfSystemOutOrSystemErr
+     */
+    private static void runInteractive() {
+        final Scanner scan = new Scanner(System.in,
+            StandardCharsets.UTF_8);
+        while (true) {
             System.out.print("> ");
             final String exprInput = scan.nextLine();
-
             if (exprInput.isBlank()) {
                 break;
             }
-
-            final CharStream codePointCharStream = CharStreams.fromString(exprInput);
-            final MathLexer lexer = new MathLexer(codePointCharStream);
-            final CommonTokenStream tokenStream = new CommonTokenStream(lexer);
-            final MathParser parser = new MathParser(tokenStream);
-
-            final MathParser.CompilationUnitContext mathTree = parser.compilationUnit();
-            final ExpressionNode ast = new MathAstBuilder().visitCompilationUnit(mathTree);
-
-            final Double value = new EvaluateExpressionVisitor().visit(ast);
-
-            final String output = String.format("= %f\n", value);
-            System.out.print(output);
-            System.out.println();
+            evaluate(exprInput);
         }
         scan.close();
     }
+
+    /**
+     * This method handles the actual evaluation of the expression ast.
+     *
+     * @param exprInput the expression to evaluate
+     * @noinspection UseOfSystemOutOrSystemErr
+     */
+    private static void evaluate(String exprInput) {
+        final CharStream codePointCharStream = CharStreams.fromString(exprInput);
+        final MathLexer lexer = new MathLexer(codePointCharStream);
+        final CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+        final MathParser parser = new MathParser(tokenStream);
+
+        final MathParser.CompilationUnitContext mathTree = parser.compilationUnit();
+        final ExpressionNode ast = new MathAstBuilder().visitCompilationUnit(mathTree);
+
+        final Double value = new EvaluateExpressionVisitor().visit(ast);
+
+        final String output = String.format("= %f\n", value);
+        System.out.print(output);
+        System.out.println();
+    }
+
+    /**
+     * Command line options.
+     *
+     * @noinspection unused, FieldMayBeFinal, CanBeFinal,
+     *     MismatchedQueryAndUpdateOfCollection, LocalCanBeFinal
+     */
+    @Command(name = "parseva-math", description = "parses mathematical expressions "
+        + "and evaluates them by traversing the ast of the expression.",
+        mixinStandardHelpOptions = true)
+    private static class CliOptions {
+
+        /**
+         * Width of CLI help option.
+         */
+        private static final int HELP_WIDTH = 100;
+
+        /**
+         * This boolean value determines whether user is in interactive mode or not.
+         */
+        @Option(names = {
+            "-i",
+            "--interact"
+        },
+            description = "Interactive mode. This option runs the parseva-math interpreter.")
+        private boolean interactiveMode;
+
+        /**
+         * This boolean value determines whether user is in evalution mode or not.
+         */
+        @Option(names = {
+            "-e",
+            "--evaluate"
+        },
+            description = "Evaluation mode. This option evaluates the given expression,"
+                + " and returns the result.",
+            defaultValue = "false")
+        private boolean evaluationMode;
+
+        /**
+         * The actual expression we are evaluating.
+         */
+        @Parameters(arity = "0..1", description = "Expression to evaluate")
+        private String expression;
+    }
+
 }
