1e8b9e010b049d562114568d77f20899e7774794
Nick Mancuso <nmancus1@dtcc.edu>
2021-01-08 02:05:12 -0500

    Issue #10: Use picoCLI to extend functionality
    Signed-off-by: Nick Mancuso <nmancus1@dtcc.edu>

--- src/main/java/parsevamath/tools/Main.java
+++ src/main/java/parsevamath/tools/Main.java
@@ -159,7 +159,7 @@ public final class Main {
             "-e",
             "--evaluate"
         },
-            description = "Evaluation mode. This option evaluates the requried expression,"
+            description = "Evaluation mode. This option evaluates the given expression,"
                 + " and returns the result.",
             defaultValue = "false")
         private boolean evaluationMode;
